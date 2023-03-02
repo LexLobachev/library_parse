4eb0e3a485f035b815a448a68cb7b09331bd308c
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-03-02 15:06:15 +0400

    download book covers

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import requests
 import os
+from urllib.parse import urljoin, urlsplit
+from urllib import parse
+
+import requests
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filepath, sanitize_filename
 
@@ -13,13 +16,28 @@ def get_book_name(book_id):
     book_tag = soup.find('td', class_='ow_px_td').find('div', id='content').find('h1')
     book_title = book_tag.text.split('::')
     book_title_text = book_title[0].strip()
+    book_img = soup.find('div', class_='bookimage').find('img')['src']
+    book_img_link = urljoin('https://tululu.org', book_img)
 
-    return book_title_text
+    return book_title_text, book_img_link
 
 
 def check_for_redirect(response):
     if response.status_code != 200:
         raise requests.HTTPError
+
+
+def download_image(url, folder='images/'):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    response = requests.get(url=url)
+    response.raise_for_status()
+
+    filename = urlsplit(parse.unquote(url)).path.split("/")[-1]
+    filepath = os.path.join(folder, filename)
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
 
 
 def download_txt(url, params, folder='books/'):
@@ -29,7 +47,8 @@ def download_txt(url, params, folder='books/'):
     response = requests.get(url=url, params=params, allow_redirects=False)
     check_for_redirect(response)
     response.raise_for_status()
-    book_name = get_book_name(params['id'])
+    book_name, book_img = get_book_name(params['id'])
+    download_image(book_img)
 
     filename = f"{params['id']}. {book_name}.txt"
     filepath = sanitize_filepath(os.path.join(folder, sanitize_filename(filename)))
@@ -54,3 +73,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    # download_image("https://tululu.org/shots/10.jpg")
