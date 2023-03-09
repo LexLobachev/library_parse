923a9f5eaed6f3b3929dc2f6b670f15af6713614
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-03-09 21:16:15 +0400

    delay catching the exception

--- parse_tululu.py
+++ parse_tululu.py
@@ -12,12 +12,7 @@ from pathvalidate import sanitize_filepath, sanitize_filename
 
 def get_book(book_id):
     url = f'https://tululu.org/b{book_id}/'
-    try:
-        response = requests.get(url, allow_redirects=True)
-    except requests.exceptions.ConnectionError:
-        print("No internet, will try to reconnect in 10 seconds")
-        time.sleep(10)
-        response = requests.get(url, allow_redirects=True)
+    response = requests.get(url, allow_redirects=True)
     check_for_redirect(response)
     response.raise_for_status()
     return response.text
@@ -57,12 +52,7 @@ def check_for_redirect(response):
 def download_image(url, folder='images/'):
     os.makedirs(folder, exist_ok=True)
 
-    try:
-        response = requests.get(url=url)
-    except requests.exceptions.ConnectionError:
-        print("No internet, will try to reconnect in 10 seconds")
-        time.sleep(10)
-        response = requests.get(url=url)
+    response = requests.get(url=url)
     response.raise_for_status()
 
     filename = urlsplit(parse.unquote(url)).path.split("/")[-1]
@@ -74,12 +64,7 @@ def download_image(url, folder='images/'):
 def download_txt(url, params, book_name, folder='books/'):
     os.makedirs(folder, exist_ok=True)
 
-    try:
-        response = requests.get(url=url, params=params, allow_redirects=True)
-    except requests.exceptions.ConnectionError:
-        print("No internet, will try to reconnect in 10 seconds")
-        time.sleep(10)
-        response = requests.get(url=url, params=params, allow_redirects=True)
+    response = requests.get(url=url, params=params, allow_redirects=True)
     check_for_redirect(response)
     response.raise_for_status()
 
@@ -106,12 +91,27 @@ def main():
         try:
             book_html = get_book(book_id)
             book = parse_book_page(book_html)
-            book_name = book['book_title']
-            book_img_url = book['book_image_url']
-            download_txt(url, params, book_name)
-            download_image(book_img_url)
+        except requests.exceptions.ConnectionError:
+            print("No internet, will try to reconnect in 10 seconds")
+            time.sleep(10)
+            book_html = get_book(book_id)
+            book = parse_book_page(book_html)
         except requests.exceptions.HTTPError:
             logging.error(f'Something went wrong with book {book_id}')
+        book_name = book['book_title']
+        book_img_url = book['book_image_url']
+        try:
+            download_txt(url, params, book_name)
+        except requests.exceptions.ConnectionError:
+            print("No internet, will try to reconnect in 10 seconds")
+            time.sleep(10)
+            download_txt(url, params, book_name)
+        try:
+            download_image(book_img_url)
+        except requests.exceptions.ConnectionError:
+            print("No internet, will try to reconnect in 10 seconds")
+            time.sleep(10)
+            download_image(book_img_url)
 
 
 if __name__ == '__main__':
